645d3030d51e0288d9b99357387c37afc9fbd83c
ibktommy <atomquartz@gmail.com>
2022-08-10 10:24:08 +0100

    Create elements on the Hero section and import closeMenuHandler function from the useGlobalState Hook

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 import phoneImg from './images/phone.svg'
 
+
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { closeMenuHandler } = useGlobalContext()
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className='hero-info'>
+          <h1>Payment Infrasctructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes - from startups to Fortune 500s-use
+            Stripe's software and APIs to accept payments, send payouts, and amnage their business online.
+          </p>
+          <button className='btn'>
+            Start Now
+          </button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt="phone" className='phone-img' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Navbar = () => {
+  
   return <h2>navbar component</h2>
 }
 

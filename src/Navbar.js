f5e44972f9fd42d26d0332268af83c10c7bea79a
ibktommy <atomquartz@gmail.com>
2022-08-10 10:48:40 +0100

    Structure the Navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,38 @@ import { FaBars } from 'react-icons/fa'
 import { useGlobalContext } from './context'
 
 const Navbar = () => {
+  const { openSideBar, openMenu, closeMenuHandler } = useGlobalContext()
   
-  return <h2>navbar component</h2>
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe" className='nav-logo' />
+          <button className="btn toggle-btn" onClick={openSideBar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">
+              Products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn">
+              Developer
+            </button>
+          </li>
+          <li>
+            <button className="link-btn">
+              Company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar

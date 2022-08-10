d59683c8dfea04d17102a02aa168b741480d0c75
ibktommy <atomquartz@gmail.com>
2022-08-10 12:31:31 +0100

    Setting the location of the submenu based on the object-values from the location-state and using useEffect to render it each time it renders

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,10 +2,18 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const { openMenu } = useGlobalContext()
+  const { openMenu, location } = useGlobalContext()
+  const container = useRef(null)
+
+  useEffect(() => {
+    const subMenu = container.current
+    const { center, bottom } = location
+    subMenu.style.left = `${center}px`
+    subMenu.style.top = `${bottom}px`
+  }, [location])
 
   return (
-    <aside className={`${openMenu ? 'submenu show' : 'submenu'}`}>
+    <aside className={`${openMenu ? 'submenu show' : 'submenu'}`} ref={container}>
       Submenu
     </aside>
   )

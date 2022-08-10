ca7857b61b7838ffba7c58a288e1262bcbc5f7aa
ibktommy <atomquartz@gmail.com>
2022-08-10 16:44:48 +0100

    Setting state to add a dynamic class to the submenu based on the content of the link

--- src/Submenu.js
+++ src/Submenu.js
@@ -3,19 +3,27 @@ import { useGlobalContext } from './context'
 
 const Submenu = () => {
   const { openMenu, location, page: { page, links } } = useGlobalContext()
+
   const container = useRef(null)
+  const [columns, setColumns] = useState('col-2')
 
   useEffect(() => {
+    setColumns('col-2')
     const subMenu = container.current
     const { center, bottom } = location
     subMenu.style.left = `${center}px`
     subMenu.style.top = `${bottom}px`
-  }, [location])
+
+    if (links.length === 3) {
+      setColumns('col-3')
+    }
+
+  }, [location, links])
 
   return (
     <aside className={`${openMenu ? 'submenu show' : 'submenu'}`} ref={container}>
       <h4>{page}</h4>
-        <div className={`submenu-center col-2`}>
+        <div className={`submenu-center ${columns}`}>
           {
             links.map((link, index) => {
               const { label, url, icon } = link
